14bca79e991ab4f84ca501e9c5687246e73e2087
al-tysevi4 <71289536+al-tysevi4@users.noreply.github.com>
2021-04-15 20:27:08 +0300

    change name

--- sandbox/src/main/java/task1/SimpleProgram.java
+++ sandbox/src/main/java/task1/SimpleProgram.java
@@ -3,7 +3,7 @@ package task1;
 public class SimpleProgram {
     public static void main(String[] args) {
       sayHello("world");
-      sayHello("Alex");
+      sayHello("Alexey");
     }
     public static void sayHello(String s) {
         System.out.println("Hello, " + s + "!");
